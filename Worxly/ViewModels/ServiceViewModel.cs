ff540e4bdabbf6771f714f65dd0992ff4d414d2b
sma02 <sma02022@outlook.com>
2024-11-06 01:43:19 +0500

    Added async image load and ItemsRepeater template for services

--- Worxly/ViewModels/ServiceViewModel.cs
+++ Worxly/ViewModels/ServiceViewModel.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Worxly.Models;
 
 namespace Worxly.ViewModels
 {
     public class ServiceViewModel : ViewModelBase
     {
+        public ObservableCollection<Service> Services { get; set; }
+        public ServiceViewModel()
+        {
+            Services = [];
+        }
     }
 }
