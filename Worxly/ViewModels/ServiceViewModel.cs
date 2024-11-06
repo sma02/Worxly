fcd7f9a5636062ecc8f4f4e55af37a534b018fbb
sma02 <sma02022@outlook.com>
2024-11-06 23:55:17 +0500

    Add service button integrated

--- Worxly/ViewModels/ServiceViewModel.cs
+++ Worxly/ViewModels/ServiceViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,15 @@ namespace Worxly.ViewModels
     public class ServiceViewModel : ViewModelBase
     {
         public ObservableCollection<Service> Services { get; set; }
+        public ReactiveCommand<Unit, Unit> AddButtonCommand { get; set; }
         public ServiceViewModel()
         {
             Services = [];
+            AddButtonCommand = ReactiveCommand.Create(AddButtonClick);
+        }
+        public void AddButtonClick()
+        {
+            throw new NotImplementedException();
         }
     }
 }
