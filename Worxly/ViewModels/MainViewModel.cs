f4af6a8088e385428b707ba58d390bd4802b547a
mwaliahmad <waliaslam2002@gmail.com>
2024-11-27 23:48:48 +0500

    Add Navigation Buttons

--- Worxly/ViewModels/MainViewModel.cs
+++ Worxly/ViewModels/MainViewModel.cs
@@ -1,16 +1,45 @@
 ﻿using ReactiveUI;
 using System;
+using System.Reactive;
+using System.Windows.Input;
 
 namespace Worxly.ViewModels;
 
 public class MainViewModel : ViewModelBase, IScreen
 {
     public RoutingState Router { get; } = new RoutingState();
+    public ReactiveCommand<Unit, IRoutableViewModel> DashboardButtonCommand { get; }
+    public ReactiveCommand<Unit, IRoutableViewModel> ServiceViewButtonCommand { get; }
+    public ReactiveCommand<Unit, IRoutableViewModel> ProfileButtonCommand { get; }
+
+    private bool isUserLoggedIn = true;
+    public bool IsUserLoggedIn
+    {
+        get => isUserLoggedIn;
+        set
+        {
+            if (value == true)
+                Globals.Instance.Router.Navigate.Execute(new UserViewModel());
+            else if (value == false)
+                Globals.Instance.Router.Navigate.Execute(new LoginViewModel());
+            this.RaiseAndSetIfChanged(ref isUserLoggedIn, value);
+        }
+    }
+
     public MainViewModel()
     {
         Globals.Instance.CurrentMainViewModel = this;
         Globals.Instance.Router = Router;
 
-        Router.Navigate.Execute(new UserViewModel());
+        IsUserLoggedIn = false;
+        DashboardButtonCommand = ReactiveCommand.CreateFromObservable(DashboardButtonClick);
+        ServiceViewButtonCommand = ReactiveCommand.CreateFromObservable(ServiceViewButtonClick);
+        ProfileButtonCommand = ReactiveCommand.CreateFromObservable(ProfileButtonClick);
+
     }
+    // navigation will be added when views will create
+    public IObservable<IRoutableViewModel> DashboardButtonClick() => Globals.Instance.Router.Navigate.Execute();
+    public IObservable<IRoutableViewModel> ServiceViewButtonClick() => Globals.Instance.Router.Navigate.Execute();
+    public IObservable<IRoutableViewModel> ProfileButtonClick() => Globals.Instance.Router.Navigate.Execute();
+
 }
